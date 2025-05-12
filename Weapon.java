eea069b6a57d46bce12e619c69a6c01ca5878e34
SiddharthJandhyala <siddharth.jandhyala0209@gmail.com>
2025-05-11 21:47:39 -0700

    Update Weapon.java

--- Weapon.java
+++ Weapon.java
@@ -1,1 +1,12 @@
+public class Weapon extends Item {
+    private int ATK;
 
+    public Weapon(String name, String description, int ATK) {
+        super(name, description);
+        this.ATK = ATK;
+    }
+
+    public int getATK() {
+        return this.ATK;
+    }
+}
