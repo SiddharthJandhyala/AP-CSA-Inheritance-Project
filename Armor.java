66373e1cfc59f43dc1f2fbd1e96bc17dc53a0a3f
SiddharthJandhyala <siddharth.jandhyala0209@gmail.com>
2025-05-11 21:44:07 -0700

    Update Armor.java

--- Armor.java
+++ Armor.java
@@ -1,1 +1,12 @@
+public class Armor extends Item {
+    private int DEF;
 
+    public Armor(String name, String description, int DEF) {
+        super(name, description);
+        this.DEF = DEF;
+    }
+
+    public int getDEF() {
+        return this.DEF;
+    }
+}
