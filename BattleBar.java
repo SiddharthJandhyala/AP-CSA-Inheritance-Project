84db6f200af427699bdc5bed0faa8016a6a59f3d
SiddharthJandhyala <siddharth.jandhyala0209@gmail.com>
2025-05-11 21:44:35 -0700

    Update BattleBar.java

--- BattleBar.java
+++ BattleBar.java
@@ -1,1 +1,16 @@
+public class BattleBar extends Dialogue {
+    private int len;
 
+    public BattleBar(int length) {
+        super("[s1][cGREY]+~");
+        this.len = length;
+    }
+
+    public void speak() {
+        for(int i = 0; i < this.len; ++i) {
+            super.speak();
+        }
+
+        (new Dialogue("[cGREY][s1]+")).speak();
+    }
+}
