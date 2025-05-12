00983eadaaf975debf662557b2ee7831a8d3de76
SiddharthJandhyala <siddharth.jandhyala0209@gmail.com>
2025-05-11 21:46:55 -0700

    Update Move.java

--- Move.java
+++ Move.java
@@ -1,1 +1,19 @@
+public class Move {
+    private String name;
+    private double damage;
+    private double boost;
 
+    public Move(String name, double damage, double boost) {
+        this.name = name;
+        this.damage = damage;
+        this.boost = boost;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public double[] getStats() {
+        return new double[]{this.damage, this.boost};
+    }
+}
