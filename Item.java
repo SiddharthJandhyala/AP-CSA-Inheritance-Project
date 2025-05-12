7b219e71c27ba6d404d37eed2df130564c635ea4
SiddharthJandhyala <siddharth.jandhyala0209@gmail.com>
2025-05-11 21:46:31 -0700

    Update Item.java

--- Item.java
+++ Item.java
@@ -1,1 +1,29 @@
+public class Item {
+    private String name;
+    private String description;
 
+    public Item(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String toString() {
+        return "name: " + this.name + " description: " + this.description;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
